5b0c1ad170459f0b20b3e1fa341573da38796489
Iago Cavalcante <iagoangelimc@gmail.com>
2018-10-25 16:55:24 -0300

    Versão para deploy

---
+++ precache-manifest.350a1e61033f8ba6def4e9f43a71812f.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb15b410cd504aed940c48b8e069cac1",
+    "url": "main.js.gz"
+  },
+  {
+    "revision": "b33d21228762b414eb1f",
+    "url": "main.js"
+  },
+  {
+    "revision": "4e5ccbaa8dc3b123419d2c407242215d",
+    "url": "index.html"
+  },
+  {
+    "url": "fee66e712a8a08eef5805a46892932ad.woff"
+  },
+  {
+    "url": "b06871f281fee6b241d60582ae9369b9.ttf"
+  },
+  {
+    "url": "af7ae505a9eed503f8b8e6982036873e.woff2"
+  },
+  {
+    "url": "912ec66d7572ff821749319396470bde.svg"
+  },
+  {
+    "url": "674f50d287a8c48dc19ba404d20fe713.eot"
+  }
+];
