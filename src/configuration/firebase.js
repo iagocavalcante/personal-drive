592ea130081ebbd6f78264fee098c417e94a90b3
Iago Cavalcante <iagoangelimc@gmail.com>
2018-10-25 16:30:17 -0300

    Configuracao relacionada ao firebase

---
+++ src/configuration/firebase.js
@@ -0,0 +1,9 @@
+import * as firebase from 'firebase';
+export const app = firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+})
