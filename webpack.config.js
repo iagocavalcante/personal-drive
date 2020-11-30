53ecf18e7374d569ada7e72ccb0c1b197f2ab28e
Iago Cavalcante <iagoangelimc@gmail.com>
2020-11-30 13:12:29 -0300

    fix: atualizando pacotes * atualizando versoes do babel * atualizando babelrc * adicionando mais variaveis de ambiente

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,59 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const Dotenv = require('dotenv-webpack')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/main.js'),
+  entry: path.resolve(__dirname, "src/main.js"),
   output: {
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    port: 9000
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 9000,
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        loader: 'babel-loader'
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
+            loader: "html-loader",
             options: {
-              minimize: true
-            }
-          }
-        ]
+              minimize: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(s*)css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(ttf|eot|svg|woff2|woff)$/,
-        loader: 'file-loader'
-      }
-    ]  
+        loader: "file-loader",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
-      filename: path.resolve(__dirname, 'dist/index.html'),
-      baseUrl: process.env.NODE_ENV == 'development'?'/':'/personal-drive/'
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: path.resolve(__dirname, "dist/index.html"),
+      baseUrl: process.env.NODE_ENV == "development" ? "/" : "/personal-drive/",
     }),
-    new CopyWebpackPlugin([
-      { from: 'static' }
-    ]),
-    new Dotenv()
-  ]
-}
+    new CopyWebpackPlugin({
+      patterns: [{ from: "static" }],
+    }),
+    new Dotenv(),
+  ],
+};
