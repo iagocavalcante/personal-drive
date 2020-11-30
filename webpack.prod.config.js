cf7422de5183114f4d586ca427b9e663150a7e4a
Iago Cavalcante <iagoangelimc@gmail.com>
2020-11-30 13:50:18 -0300

    fix: adicionando carregamento de variavel de ambiente em producao

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,6 +5,7 @@ const WorkboxPlugin = require('workbox-webpack-plugin')
 const webpack = require('webpack')
 const rimraf = require('rimraf')
 const fs = require('fs')
+const Dotenv = require("dotenv-webpack");
 
 if (fs.existsSync('./dist')) {
   rimraf('./dist', () => console.log('./dist removed'))
